40a933f72a9d5539c73ac93bc600ededdd568a88
Joshua Hwang <joshua2hwang2@gmail.com>
2026-04-28 16:24:15 -0500

    updated database

--- RateThatMovieGUI/data importer/db_info_test.py
+++ RateThatMovieGUI/data importer/db_info_test.py
@@ -1,7 +1,7 @@
 #FOR TESTING
-DBUSER = "hwaji-28"
-DBPASS = "hwaji-28"
+#DBUSER = "hwaji-28"
+#DBPASS = "hwaji-28"
 
 #IF YOU WANT TO IMPORT TO THE REAL DATABASE
-#DBUSER = "watmj-26"
-#DBPASS = "watmj-26"
+DBUSER = "watmj-26"
+DBPASS = "watmj-26"
--- RateThatMovieGUI/page_search.py
+++ RateThatMovieGUI/page_search.py
@@ -13,13 +13,23 @@ def search():
 
     #add sm code here
     with ui.card():
-        ui.label("Search Movies, Actors, and More!").style('font-size: 200%')
+        ui.label("Search Movies, Cast, and More!").style('font-size: 200%')
 
         with ui.row():
             ui.button("Movies")
             ui.button("Actors")
 
+
     uit_footnote()
+
+
+def search_movies():
+    cols = [{'name': 'movie_title', 'label': 'Movie Title', 'field': 'movie_title', 'align': 'left'},
+            {'name': 'movie_release_date', 'label': 'Release Date', 'field': 'movie_release_date', 'align': 'left'}]
+    search_table = ui.table(columns=cols, rows=[], pagination={'rowsPerPage': 30, 'sortBy': 'movie_title'})
+
+
+    #call database
 
 #set table up to search for movies
 def set_table_movies():
